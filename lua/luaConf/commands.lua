d88c47b4ef84084db8746d932aaa1f0c444df465
baileybrightman <baileybrightman@gmail.com>
2023-11-07 13:20:02 -0400

    formatting, tabbing changed on several filetypes, made java config respect other users

--- lua/luaConf/commands.lua
+++ lua/luaConf/commands.lua
@@ -1,5 +1,3 @@
-
-
 -- When the filetype is improperly detected, fix the issue and restart the LSP
 vim.api.nvim_create_user_command('HeaderC', 'set filetype=c | LspRestart', {})
 vim.api.nvim_create_user_command('HeaderCpp', 'set filetype=cpp | LspRestart', {})
