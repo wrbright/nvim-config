e0a20717533d0d216fc37385e443818bfa07a57c
baileybrightman <baileybrightman@gmail.com>
2023-11-07 13:20:21 -0400

    Made java config respect other users

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -4,16 +4,16 @@ local config = {
 }
 
 local bundles = {
-    vim.fn.glob("/home/m/InstalledApplicationsDev/java-debug/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-*", 1)
+    vim.fn.glob("$HOME/InstalledApplicationsDev/java-debug/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-*", 1)
 };
 
-vim.list_extend(bundles, vim.split(vim.fn.glob("/home/m/InstalledApplicationsDev/vscode-java-test/server/*.jar", 1), "\n"))
+vim.list_extend(bundles, vim.split(vim.fn.glob("$HOME/InstalledApplicationsDev/vscode-java-test/server/*.jar", 1), "\n"))
 config['init_options'] = {
     bundles = bundles;
 }
 
 vim.cmd'set expandtab'
-config['on_attach'] = require("luaConf/on_attach")
+config['on_attach'] = require'luaConf/on_attach'
 
 
 require'jdtls'.start_or_attach(config)
