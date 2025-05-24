c751c8322685bf5dbfad40aa2e3126bd281805c9
baileybrightman <baileybrightman@gmail.com>
2025-05-24 00:06:52 -0300

    CR user command nolonger depends on plugin
    Plugin was removed, now runs Cargo Run as a system command with !cargo
    run

--- lua/luaConf/commands.lua
+++ lua/luaConf/commands.lua
@@ -1,4 +1,5 @@
 -- When the filetype is improperly detected, fix the issue and restart the LSP
 vim.api.nvim_create_user_command('HeaderC', 'set filetype=c | LspRestart', {})
 vim.api.nvim_create_user_command('HeaderCpp', 'set filetype=cpp | LspRestart', {})
-vim.api.nvim_create_user_command('Cr', 'Cargo run', {})
+vim.api.nvim_create_user_command('Cr', '!cargo run', {})
+vim.api.nvim_create_user_command('Cargo run', '!cargo run', {})
